83dfcb61464013acd7ff2d1d0a5a7f948bd49163
mateoGom <matemgm@hotmail.com>
2021-04-15 23:33:47 +0200

    se agrega RunnerTags

---
+++ src/test/java/runners/RunnerTags.java
@@ -0,0 +1,16 @@
+package runners;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features/registroUtest.feature",
+        tags = "@scenario1",
+        glue = "stepDefinitions",
+        snippets = SnippetType.CAMELCASE )
+
+public class RunnerTags {
+}
