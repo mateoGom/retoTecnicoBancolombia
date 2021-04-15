e06e23f9588da4436a866bb0ce111ea3c4420512
mateoGom <matemgm@hotmail.com>
2021-04-15 23:54:31 +0200

    se agrega logica para abrir pagina

---
+++ src/main/java/tasks/OpenUp.java
@@ -0,0 +1,19 @@
+package tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Open;
+import userInterface.UtestPage;
+
+public class OpenUp implements Task {
+    private UtestPage utestPage;
+    public static OpenUp thePage() {
+        return Tasks.instrumented(OpenUp.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor){
+        actor.attemptsTo(Open.browserOn(utestPage));
+    }
+}
--- src/test/java/stepDefinitions/registroUtestStepDefinitions.java
+++ src/test/java/stepDefinitions/registroUtestStepDefinitions.java
@@ -2,15 +2,23 @@ package stepDefinitions;
 
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import tasks.OpenUp;
 
 public class registroUtestStepDefinitions {
+    @Before
+    public void setStage(){
+        OnStage.setTheStage(new OnlineCast());
+    }
+
     @Given("^than Mateo wants to enter the page$")
-    public void thanMateoWantsToEnterThePage() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void thanMateoWantsToEnterThePage()  {
+       OnStage.theActorCalled("Mateo").wasAbleTo(OpenUp.thePage());
     }
 
     @When("^He enters his datas$")
