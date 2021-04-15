03025849d4b711ee67d248aa029af5b09694b7b2
mateoGom <matemgm@hotmail.com>
2021-04-15 23:40:39 +0200

    se agrega step definitions

--- src/test/java/stepDefinitions/registroUtestStepDefinitions.java
+++ src/test/java/stepDefinitions/registroUtestStepDefinitions.java
@@ -1,4 +1,33 @@
 package stepDefinitions;
 
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class registroUtestStepDefinitions {
+    @Given("^than Mateo wants to enter the page$")
+    public void thanMateoWantsToEnterThePage() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^He enters his datas$")
+    public void heEntersHisDatas(DataTable arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
+    }
+
+    @Then("^He finds the message$")
+    public void heFindsTheMessage(DataTable arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
+    }
 }
